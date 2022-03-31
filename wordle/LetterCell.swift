f130250a93bdd77759e35f6eba0d101b54d7307a
Tom Raybould <Thomaseraybould@gmail.com>
2022-03-30 21:29:21 -0400

    Loading with error

---
+++ wordle/LetterCell.swift
@@ -0,0 +1,16 @@
+//
+//  LetterCell.swift
+//  wordle
+//
+//  Created by Thomas Raybould on 3/30/22.
+//
+
+import Foundation
+import UIKit
+
+class LetterCell: UICollectionViewCell{
+
+    
+    @IBOutlet weak var letterValue: UILabel!
+    
+}
--- wordle/ViewController.swift
+++ wordle/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    
+    let testData = Array(0...4)
+    
+    @IBOutlet var wordleCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        wordleCollectionView.delegate = self
+        wordleCollectionView.dataSource = self
     }
-
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+    
+        return testData.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "test", for: indexPath as IndexPath) as! LetterCell
+        
+       
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
 
 }
 
