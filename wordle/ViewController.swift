d4cb8b11e78c44d9916d4f35ada5ec002cc91bc0
Tom Raybould <Thomaseraybould@gmail.com>
2022-04-03 14:53:04 -0400

    5 equally sized boxes

--- wordle/ViewController.swift
+++ wordle/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     let testData = Array(0...4)
+    let ITEMS_PER_ROW = 5
     
     @IBOutlet weak var wordleCollectionView: UICollectionView!
     
@@ -17,11 +18,11 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         super.viewDidLoad()
         print("viewDidLoad")
         wordleCollectionView.dataSource = self
+        wordleCollectionView.delegate = self
     }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
-        
         return testData.count
     }
     
@@ -33,6 +34,23 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let numberOfItemsPerRow:CGFloat = 5
+        let spacingBetweenCells:CGFloat = 16
+        let spacing:CGFloat = 16.0
+                
+        let totalSpacing = (2 * spacing) + ((numberOfItemsPerRow - 1) * spacingBetweenCells)
+                
+        if let collection = self.wordleCollectionView{
+            let width = (collection.bounds.width - totalSpacing)/numberOfItemsPerRow
+            return CGSize(width: width, height: width)
+        }else{
+            return CGSize(width: 0, height: 0)
+        }
+    }
+        
     
 }
 
